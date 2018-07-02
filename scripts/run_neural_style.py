dd8a650361b1a7550be892d3b0c76e5df3a28e42
Ilia E <hawk.sayman@gmail.com>
2018-07-02 11:31:16 +0300

    run_several_neural_style

---
+++ scripts/run_neural_style.py
@@ -0,0 +1,5 @@
+import os
+
+os.system("python neural_style.py --content_img golden_gate.jpg --style_imgs starry-night.jpg --max_size 1000 \
+--max_iterations 10 --original_colors --img_name 10_iter.jpg")
+
